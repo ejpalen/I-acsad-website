462b4259c89bdd8e0144f79b8d38822f27dd17a1
Ej Palen <92970626+ejpalen@users.noreply.github.com>
2022-03-03 11:23:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,11 +12,11 @@ var NSTPActsHTML = document.getElementById("nstp-activities");
 
 var webApp = "<h3>Quiz [Due:Thurs]</h3>";
 var comProg = "<h3>Asynchronous Activity (Table)</h3> ";
-var sts ="<h3>Quiz in TBL [Resched]</h3><h3>STEP 3: Accomplish the Student Contract for Flexible Learning</h3>";
+var sts ="<h3>Quiz in TBL [Due: Thurs]</h3><h3>STEP 3: Accomplish the Student Contract for Flexible Learning</h3>";
 var purcom = "<h3>Quiz [Fri]</h3>";
 var fildis = "<h3></h3>";
 var ssp = "<h3>Quiz 1 [Due: Thurs]</h3><h3>EM-at3.  Market Structures [Due: Fri]</h3>";
-var infoman = "<h3>Quiz in TBL</h3>";
+var infoman = "<h3>Quiz in TBL</h3><h3>Asynchronous Activity (Table)</h3>";
 var probstat = "<h3>Activity 1 - Simple Probability</h3>";
 var fitness = "<h3></h3>";
 var nstp = "<h3></h3>";
@@ -28,7 +28,7 @@ var thereIsPurcom = true;
 var thereIsFildis = false;
 var thereIsSSP = true;
 var thereIsInfoman = true;
-var thereIsProbstat = true;
+var thereIsProbstat = false;
 var thereIsFitness = false;
 var thereIsNstp = false;
 
@@ -112,7 +112,7 @@ if (thereIsNstp) {
   NSTPActsHTML.innerHTML = "None";
 }
 
-var updateTime = "<p>Updated: 3/2/2022 9:55 PM</p>";
+var updateTime = "<p>Updated: 3/3/2022 10:23 PM</p>";
 
 acts.innerHTML = "";
 acts.innerHTML =
