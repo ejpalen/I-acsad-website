ccccb1349f1b10122b40ca9b249917c6d42df1b5
Ej Palen <92970626+ejpalen@users.noreply.github.com>
2022-03-07 17:36:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -11,24 +11,24 @@ var FitnessActsHTML = document.getElementById("fitness-activities");
 var NSTPActsHTML = document.getElementById("nstp-activities");
 
 var webApp = "<h3></h3>";
-var comProg = "<h3></h3> ";
-var sts ="<h3>STEP 3: Accomplish the Student Contract for Flexible Learning</h3>";
-var purcom = "<h3></h3>";
+var comProg = "<h3>Seatwork 1 [Due: Thurs]</h3> ";
+var sts ="<h3></h3>";
+var purcom = "<h3>Quiz [Tues]</h3>";
 var fildis = "<h3></h3>";
-var ssp = "<h3>EM-at3.  Market Structures [Due: Fri]</h3>";
-var infoman = "<h3>Quiz in TBL</h3><h3>Asynchronous Activity (Table)</h3>";
-var probstat = "<h3></h3>";
+var ssp = "<h3></h3>";
+var infoman = "<h3>Asynchronous Activity (Table)</h3>";
+var probstat = "<h3>Activity 2 & 3 [Due: Wed]</h3>";
 var fitness = "<h3></h3>";
 var nstp = "<h3></h3>";
 
 var thereIsWeb = false;
-var thereIsComprog = false;
-var thereIsSts = true;
-var thereIsPurcom = false;
+var thereIsComprog = true;
+var thereIsSts = false;
+var thereIsPurcom = true;
 var thereIsFildis = false;
-var thereIsSSP = true;
+var thereIsSSP = false;
 var thereIsInfoman = true;
-var thereIsProbstat = false;
+var thereIsProbstat = true;
 var thereIsFitness = false;
 var thereIsNstp = false;
 
@@ -112,7 +112,7 @@ if (thereIsNstp) {
   NSTPActsHTML.innerHTML = "None";
 }
 
-var updateTime = "<p>Updated: 3/4/2022 4:58 PM</p>";
+var updateTime = "<p>Updated: 3/7/2022 5:35 PM</p>";
 
 acts.innerHTML = "";
 acts.innerHTML =
